d715a386aa0016dc6fcb6f5302d0c013d6ee6c68
Willian Prado <willian_prado@hotmail.com>
2022-01-22 02:28:48 -0300

    Add form listener to send messages

--- views/webchat.js
+++ views/webchat.js
@@ -1,12 +1,35 @@
 const socket = window.io();
 
+let nickname = '';
 const nicknameElement = document.querySelector('#user-nickname');
 
-const setClientNickname = (nickname) => {
-  nicknameElement.innerText = nickname;
+const setClientNickname = (username) => {
+  nicknameElement.innerText = username;
+  nickname = username;
+};
+
+const ulMessageList = document.querySelector('#message-list');
+const messageForm = document.querySelector('#message-form');
+const inputMessage = document.querySelector('#message-box');
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const chatMessage = inputMessage.value;
+  socket.emit('message', { chatMessage, nickname });
+  inputMessage.value = '';
+  return false;
+});
+
+const createChatMessage = (message) => {
+  const li = document.createElement('li');
+  li.innerText = message;
+  li.setAttribute('data-testid', 'message');
+  ulMessageList.appendChild(li);
 };
 
 socket.on('welcome', (_message) => {
-  const nickname = socket.id.substring(0, 16);
-  setClientNickname(nickname);
+  const randomName = socket.id.substring(0, 16);
+  setClientNickname(randomName);
 });
+
+socket.on('message', (message) => createChatMessage(message));
