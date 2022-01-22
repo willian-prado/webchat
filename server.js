8ef8388cc5b6999939cc84ae9a040092a2108e3a
Willian Prado <willian_prado@hotmail.com>
2022-01-21 21:51:18 -0300

    Initial commit

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const httpServer = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: `https://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => res.render('index'));
+
+httpServer.listen(PORT, () => console.log(`Socket.io server is listening on PORT ${PORT}`));
